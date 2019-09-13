4e28868516f0f56c5d2381941e5a61ba6873db9b
katerina <42969775+edubinskaya18214@users.noreply.github.com>
2019-09-13 15:57:27 +0700

    Add task 1

---
+++ task 1/src/test/java/tests.java
@@ -0,0 +1,34 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class tests {
+    private HeapSort gen = new HeapSort();
+    @Test
+    public void test1() {
+        int[] test_arr = {-2, -5, -8, -255, -8, -78, 6, 24, -20};
+        int[] check_arr = {-255, -78, -20, -8, -8, -5, -2, 6, 24};
+        Assert.assertArrayEquals(check_arr, gen.heapSort(test_arr));
+        System.out.println("test 1 is successful");
+    }
+    @Test
+    public void test2(){
+        int[] test_arr = {10,9,8,7,6,5,4,3,2,1};
+        int[] check_arr = {1,2,3,4,5,6,7,8,9,10};
+        Assert.assertArrayEquals(check_arr, gen.heapSort(test_arr));
+        System.out.println("test 2 is successful");
+    }
+    @Test
+    public void test3(){
+        int[] test_arr = {0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+        int[] check_arr = {-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+        Assert.assertArrayEquals(check_arr, gen.heapSort(test_arr));
+        System.out.println("test 3 is successful");
+    }
+    @Test
+    public void test4(){
+        int[] test_arr = {100000000, -300000, -1000000000};
+        int[] check_arr = {-1000000000, -300000, 100000000};
+        Assert.assertArrayEquals(check_arr, gen.heapSort(test_arr));
+        System.out.println("test 4 is successful");
+    }
+}
