de0fcf1e1d55ff407d5db78147ca69276ec0894f
katerina <42969775+edubinskaya18214@users.noreply.github.com>
2019-09-16 17:09:08 +0700

    added beta-v of t1.1 t1.2
    added t1.2 and rename t1.1

---
+++ task 1.2/src/main/java/algoritm.java
@@ -0,0 +1,26 @@
+
+public class algoritm {
+    private int[][] g;
+    private int v;
+
+    public void floydWarshall(int[][] d, int n) {
+        v = n;
+        setGraph(d);
+        for (int k = 0; k < v; ++k)
+            for (int i = 0; i < v; ++i)
+                for (int j = 0; j < v; ++j)
+                    if ( g[i][k] != -1 && g[j][k] != -1 && g[i][j] > g[i][k] + g[k][j] && i != j)
+                        g[i][j] = g[i][k] + g[k][j];
+    }
+
+    private void setGraph(int[][] d) {
+        g = new int[v][v];
+        for (int i = 0; i < v; ++i)
+            for (int j = 0; j < v; ++j)
+                g[i][j] = d[i][j];
+    }
+
+    public int takeDist(int v1, int v2){
+        return g[v1][v2];
+    }
+}
