2b365bf4d97cb535f10e6ce26e4738d4fe2f3bbc
katerina <42969775+edubinskaya18214@users.noreply.github.com>
2019-09-19 12:03:38 +0700

    ++

---
+++ task 1.3/src/main/java/ru/nsu/fit/g18214/dubinskaya/findSubStr.java
@@ -0,0 +1,22 @@
+package ru.nsu.fit.g18214.dubinskaya;
+
+import java.util.Scanner;
+
+public class findSubStr {
+
+    findSubStr(String s, String sub){
+        zFunction zf = new zFunction();
+        int[] arr = zf.returnNumSubs(s, sub);
+        for(int i = 0; i < arr.length; ++i){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args){
+        String str, substr;
+        Scanner scanner = new Scanner(System.in);
+        str = scanner.nextLine();
+        substr = scanner.nextLine();
+        findSubStr f = new findSubStr(str, substr);
+    }
+}
---
+++ task 1.3/src/test/java/ru/nsu/fit/g18214/dubinskaya/tests.java
@@ -0,0 +1,68 @@
+package ru.nsu.fit.g18214.dubinskaya;
+
+import org.junit.Assert;
+import org.junit.Test;
+import ru.nsu.fit.g18214.dubinskaya.zFunction;
+
+public class tests {
+    private zFunction gen = new zFunction();
+    @Test
+    public void test1() {
+        String s = "Я, конечно, люблю яблоки, но я думаю, они меня преследуют";
+        String sub = "Я, конечно, люблю яблоки, но я думаю, они меня преследуют";
+        int[] checkArr = {0};
+        Assert.assertArrayEquals(checkArr, gen.returnNumSubs(s,sub));
+        System.out.println("test 1 is successful");
+    }
+    @Test
+    public void test2(){
+                  //0123456789012345678901234567890123456789012345678901234567
+        String s = "Я, конечно, люблю яблоки, но я думаю, они меня преследуют";
+        String sub = "яблоки";
+        int[] checkArr = {18};
+        Assert.assertArrayEquals(checkArr, gen.returnNumSubs(s,sub));
+        System.out.println("test 2 is successful");
+    }
+    @Test
+    public void test3(){
+                  //0123456789012345678901234567890123456789012345678901234567
+        String s = "Я, конечно, люблю яблоки, но я думаю, они меня преследуют";
+        String sub = "Карл у Клары украл кораллы, а Клара у Карла украла Кларнет, жалко мне клару, как она будет без кораллов, хоспаде, что я пишу";
+        int[] checkArr = {};
+        Assert.assertArrayEquals(checkArr, gen.returnNumSubs(s,sub));
+        System.out.println("test 3 is successful");
+    }
+    @Test
+    public void test4(){
+                  //0123456789012345678901234567890123456789012345678901234567
+        String s = "Я, конечно, люблю яблоки, но я думаю, они меня преследуют";
+        String sub = "Я, конечно, люблю яблоки, но я думаю, они меня преследуют. Хотя, наверное, неплохо иметь поклонников";
+        int[] checkArr = {};
+        Assert.assertArrayEquals(checkArr, gen.returnNumSubs(s,sub));
+        System.out.println("test 4 is successful");
+    }
+    @Test
+    public void test5(){
+                  //0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789
+        String s = "аааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааа" +
+                "аааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааа" +
+                "аааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааа" +
+                "аааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааа" +
+                "аааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааа" +
+                "аааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааа";
+        String sub = "а";
+        int[] checkArr = new int[600];
+        for (int i = 0; i < 600; ++i)
+            checkArr[i] = i;
+        Assert.assertArrayEquals(checkArr, gen.returnNumSubs(s,sub));
+        System.out.println("test 5 is successful");
+    }
+    @Test
+    public void test6(){
+        String s = "";
+        String sub = "Карл у Клары украл кораллы";
+        int[] checkArr = {};
+        Assert.assertArrayEquals(checkArr, gen.returnNumSubs(s,sub));
+        System.out.println("test 3 is successful");
+    }
+}
