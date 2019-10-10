1b3901323e60e6237eccbc3938f334681c0d733a
Це я <bromkate@mail.ru>
2019-10-10 20:01:20 +0700

    delete Task_1_2

--- Task_1_2/src/main/java/ru/nsu/fit/g18214/dubinskaya/NeighboringVertices.java
+++
@@ -1,8 +0,0 @@
-package ru.nsu.fit.g18214.dubinskaya;
-
-import java.util.*;
-
-public class NeighboringVertices {
-  public List<Integer> edges = new ArrayList<Integer>();
-  public List<Integer> dist = new ArrayList<Integer>();
-}
